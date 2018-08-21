d0a2a7d8f8505a02b58ce0e0d71910f6417c7e97
unknown <huyho95@gmail.com>
2018-08-21 17:10:39 +0700

    my homework

---
+++ 2018-08-21/18082018(3).js
@@ -0,0 +1,9 @@
+function anHiendiv(){
+    var div = document.getElementById('js-div');
+    if(div.style.display === 'none'){
+        div.style.display='block';  
+    }else{
+        div.style.display='none';
+    }    
+
+}
